f61a6ad88c0f1388f2018ccd2c60d6ca3b6666ca
Zander Franks <me@zanderf.net>
2024-03-11 16:42:48 -0500

    feat(cli): ledger tx command, add command, bulk tx gen + add to blocks
    Signed-off-by: Zander Franks <me@zanderf.net>

--- cli/src/commands/ledger.rs
+++ cli/src/commands/ledger.rs
@@ -15,11 +15,11 @@
 use std::{fs, path::PathBuf, str::FromStr};
 
 use aleo_std::StorageMode;
-use anyhow::Result;
+use anyhow::{ensure, Result};
 use clap::{Args, Subcommand};
-use indexmap::IndexMap;
-use rand::{Rng, SeedableRng};
+use rand::SeedableRng;
 use rand_chacha::ChaChaRng;
+use serde::Deserialize;
 use snarkvm::{
     circuit::{Aleo, AleoV0},
     console::{
@@ -31,9 +31,7 @@ use snarkvm::{
     ledger::{
         query::Query,
         store::{helpers::rocksdb::ConsensusDB, ConsensusStorage},
-        Block,
-        Ledger,
-        Transaction,
+        Block, Ledger, Transaction,
     },
     synthesizer::{process::execution_cost, VM},
     utilities::FromBytes,
@@ -47,6 +45,21 @@ pub struct Command {
     pub command: Commands,
 }
 
+#[derive(Debug, Deserialize, Clone)]
+pub struct TxOperation {
+    from: PrivateKey<MainnetV0>,
+    to: Address<MainnetV0>,
+    amount: u32,
+}
+
+impl FromStr for TxOperation {
+    type Err = serde_json::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        serde_json::from_str(s)
+    }
+}
+
 #[derive(Debug, Subcommand)]
 pub enum Commands {
     Init {
@@ -57,6 +70,16 @@ pub enum Commands {
         #[arg(required = true, short, long)]
         output: PathBuf,
     },
+    Tx {
+        #[arg(required = true, short, long, default_value = "./genesis.block")]
+        genesis: PathBuf,
+        #[arg(required = true, short, long)]
+        ledger: PathBuf,
+        #[arg(required = true, long)]
+        operations: Vec<TxOperation>,
+        #[arg(required = true, short, long)]
+        output: PathBuf,
+    },
     Add {
         /// A path to the genesis block to initialize the ledger from.
         #[arg(required = true, short, long, default_value = "./genesis.block")]
@@ -64,14 +87,18 @@ pub enum Commands {
         /// A destination path for the ledger directory.
         #[arg(required = true, short, long)]
         ledger: PathBuf,
-        /// The seed to use when generating a new block. Defaults to the dev mode seed.
+        /// The seed to use when generating blocks. Defaults to the dev mode seed.
         #[arg(name = "seed", long)]
         seed: Option<u64>,
-        /// The number of blocks to add  to use when generating a new block. Defaults to the dev mode seed.
-        #[arg(short, long, default_value_t = 1)]
-        num: u8,
-        #[arg(short, long, default_value = "./accounts.json")]
-        accounts_file: PathBuf,
+        /// The private key to use when generating the block.
+        #[arg(name = "private-key", long)]
+        private_key: PrivateKey<MainnetV0>,
+        /// The number of transactions to add per block.
+        #[arg(name = "txns-per-block", long)]
+        txns_per_block: Option<usize>,
+        /// The transactions file to read from. Should have been generated with `snarkos ledger tx`.
+        #[arg(name = "txns-file")]
+        txns_file: PathBuf,
     },
     View {
         /// A path to the genesis block to initialize the ledger from.
@@ -83,41 +110,6 @@ pub enum Commands {
         /// The block height to view.
         block_height: u32,
     },
-    Cannon {
-        /// A path to the genesis block to initialize the ledger from.
-        #[arg(required = true, short, long, default_value = "./genesis.block")]
-        genesis: PathBuf,
-        /// The ledger from which to view a block.
-        #[arg(required = true, short, long)]
-        ledger: PathBuf,
-    },
-}
-
-struct Accounts<N: Network>(pub IndexMap<Address<N>, (PrivateKey<N>, u64)>);
-struct Account<N: Network> {
-    addr: Address<N>,
-    pk: PrivateKey<N>,
-    // TODO see if this is tracked anywhere
-    balance: u64,
-}
-
-impl<N: Network> Accounts<N> {
-    fn from_file(path: PathBuf) -> Result<Self> {
-        let accounts: IndexMap<Address<N>, (PrivateKey<N>, u64)> = serde_json::from_reader(fs::File::open(path)?)?;
-        Ok(Self(accounts))
-    }
-
-    fn two_random_accounts<'a>(&self, rng: &mut ChaChaRng) -> (Account<N>, Account<N>) {
-        let len = self.0.len();
-        let first_index = rng.gen_range(0..len);
-        let second_index = rng.gen_range(0..len);
-
-        let (addr1, (pk1, balance1)) = self.0.get_index(first_index).unwrap();
-        // TODO could also be a random new account
-        let (addr2, (pk2, balance2)) = self.0.get_index(second_index).unwrap();
-
-        (Account { addr: *addr1, pk: *pk1, balance: *balance1 }, Account { addr: *addr2, pk: *pk2, balance: *balance2 })
-    }
 }
 
 impl Commands {
@@ -133,7 +125,28 @@ impl Commands {
                 Ledger::<_, ConsensusDB<MainnetV0>>::load(genesis_block, StorageMode::Custom(output))?;
                 Ok(String::from("Ledger written"))
             }
-            Commands::Add { genesis, ledger, seed, num, accounts_file } => {
+
+            Commands::Tx { genesis, ledger, operations, output } => {
+                // Read the genesis block
+                let genesis_block = Block::<MainnetV0>::read_le(fs::File::open(genesis)?)?;
+
+                // Load the ledger
+                let ledger = Ledger::<_, ConsensusDB<MainnetV0>>::load(genesis_block, StorageMode::Custom(ledger))?;
+
+                let txns = operations
+                    .into_iter()
+                    .map(|op| make_transaction_proof::<_, _, AleoV0>(ledger.vm(), op.to, op.amount, op.from, None))
+                    .collect::<Result<Vec<_>>>()?;
+
+                let file = fs::File::options().append(false).create(true).write(true).open(&output)?;
+                serde_json::to_writer_pretty(file, &txns)?;
+
+                Ok(format!("Wrote {} transactions to {}.", txns.len(), output.display()))
+            }
+
+            Commands::Add { genesis, ledger, seed, private_key, txns_per_block, txns_file } => {
+                type Vm = VM<MainnetV0, ConsensusDB<MainnetV0>>;
+
                 let mut rng = ChaChaRng::seed_from_u64(seed.unwrap_or(DEVELOPMENT_MODE_RNG_SEED));
 
                 // Read the genesis block
@@ -142,11 +155,35 @@ impl Commands {
                 // Load the ledger
                 let ledger = Ledger::<_, ConsensusDB<MainnetV0>>::load(genesis_block, StorageMode::Custom(ledger))?;
 
-                // Load the accounts
-                let mut accounts = Accounts::from_file(accounts_file)?;
-                (0..num).try_for_each(|_| add_block::<_, AleoV0>(&mut rng, &ledger, &mut accounts))?;
-                Ok(format!("Inserted {num} block(s) into ledger"))
+                // Ensure we aren't trying to stick too many transactions into a block
+                let per_block = txns_per_block.unwrap_or(Vm::MAXIMUM_CONFIRMED_TRANSACTIONS);
+                ensure!(
+                    per_block <= Vm::MAXIMUM_CONFIRMED_TRANSACTIONS,
+                    "too many transactions per block (max is {})",
+                    Vm::MAXIMUM_CONFIRMED_TRANSACTIONS
+                );
+
+                // Load the transactions
+                let txns: Vec<Transaction<MainnetV0>> = serde_json::from_reader(fs::File::open(txns_file)?)?;
+
+                // Add the appropriate number of blocks
+                let mut block_count = 0;
+                for chunk in txns.chunks(per_block) {
+                    let target_block = ledger.prepare_advance_to_next_beacon_block(
+                        &private_key,
+                        vec![],
+                        vec![],
+                        chunk.to_vec(),
+                        &mut rng,
+                    )?;
+
+                    ledger.advance_to_next_block(&target_block)?;
+                    block_count += 1;
+                }
+
+                Ok(format!("Inserted {block_count} blocks into the ledger."))
             }
+
             Commands::View { genesis, ledger, block_height } => {
                 // Read the genesis block
                 let genesis_block = Block::<MainnetV0>::read_le(fs::File::open(genesis)?)?;
@@ -157,14 +194,13 @@ impl Commands {
                 // Print information about the ledger
                 Ok(format!("{:#?}", ledger.get_block(block_height)?))
             }
-            Commands::Cannon { .. } => Ok(String::from("TODO")),
         }
     }
 }
 
 pub fn make_transaction_proof<N: Network, C: ConsensusStorage<N>, A: Aleo<Network = N>>(
     vm: &VM<N, C>,
-    address: String,
+    address: Address<N>,
     amount: u32,
     private_key: PrivateKey<N>,
     private_key_fee: Option<PrivateKey<N>>,
@@ -186,7 +222,11 @@ pub fn make_transaction_proof<N: Network, C: ConsensusStorage<N>, A: Aleo<Networ
             &private_key,
             ProgramID::from_str("credits.aleo")?,
             Identifier::from_str("transfer_public")?,
-            vec![Value::from_str(&address)?, Value::from(Literal::U64(U64::new(amount_microcredits)))].into_iter(),
+            vec![
+                Value::from_str(address.to_string().as_str())?,
+                Value::from(Literal::U64(U64::new(amount_microcredits))),
+            ]
+            .into_iter(),
             rng,
         )?;
 
@@ -215,24 +255,4 @@ pub fn make_transaction_proof<N: Network, C: ConsensusStorage<N>, A: Aleo<Networ
 
     // assemble the transaction
     Transaction::<N>::from_execution(execution, Some(fee))
-}
-
-fn add_block<N: Network, A: Aleo<Network = N>>(
-    rng: &mut ChaChaRng,
-    ledger: &Ledger<N, ConsensusDB<N>>,
-    accounts: &mut Accounts<N>,
-) -> Result<()> {
-    let (acc1, acc2) = accounts.two_random_accounts(rng);
-
-    let tx = make_transaction_proof::<_, _, A>(ledger.vm(), acc1.addr.to_string(), 1_000, acc2.pk, None)?;
-    let target_block = ledger.prepare_advance_to_next_beacon_block(&acc1.pk, vec![], vec![], vec![tx], rng)?;
-
-    println!("Generated block hash: {}", target_block.hash());
-    println!("New block height: {}", target_block.height());
-    println!("New block solutions: {:?}", target_block.solutions());
-
-    // Insert the block into the ledger's block store
-    ledger.advance_to_next_block(&target_block)?;
-
-    Ok(())
 }
