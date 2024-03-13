e8e638a307d11bbcb49d430f517b36557d7b090f
Zander Franks <me@zanderf.net>
2024-03-13 13:34:50 -0500

    feat(cli): address generation in ledger distribute command
    Signed-off-by: Zander Franks <me@zanderf.net>

--- cli/src/commands/ledger/mod.rs
+++ cli/src/commands/ledger/mod.rs
@@ -14,7 +14,7 @@
 
 use std::{ops::Deref, path::PathBuf, str::FromStr};
 
-use anyhow::{ensure, Result};
+use anyhow::{bail, ensure, Result};
 use clap::{Args, Subcommand};
 use indicatif::{ParallelProgressIterator, ProgressBar, ProgressIterator};
 use rand::{seq::SliceRandom, thread_rng, CryptoRng, Rng, SeedableRng};
@@ -198,8 +198,17 @@ pub enum Commands {
         /// The ledger from which to view a block.
         #[arg(required = true, short, long)]
         ledger: PathBuf,
+        /// The private key in which to distribute credits from.
+        #[arg(required = true, long)]
         from: PrivateKey<Network>,
-        to: Accounts,
+        /// A comma-separated list of addresses to distribute credits to. This or `--num-accounts` must be passed.
+        #[arg(long)]
+        to: Option<Accounts>,
+        /// The number of new addresses to generate and distribute credits to. This or `--to` must be passed.
+        #[arg(long)]
+        num_accounts: Option<u32>,
+        /// The amount of microcredits to distribute.
+        #[arg(long)]
         amount: u64,
     },
 }
@@ -442,12 +451,29 @@ impl Commands {
                 Ok(format!("{address} balance {}", util::get_balance(addr, &ledger)?))
             }
 
-            Commands::Distribute { genesis, ledger, from, to, amount } => {
+            Commands::Distribute { genesis, ledger, from, to, num_accounts, amount } => {
                 let ledger = util::open_ledger::<Network, Db>(genesis, ledger)?;
                 let mut rng = ChaChaRng::from_entropy();
 
+                // Determine the accounts to distribute to
+                let to = match (to, num_accounts) {
+                    // Addresses explicitly passed
+                    (Some(to), None) => to.0,
+
+                    // No addresses passed, generate private keys at runtime
+                    (None, Some(num)) => (0..num)
+                        .into_iter()
+                        .map(|_| Address::try_from(PrivateKey::<Network>::new(&mut rng)?))
+                        .collect::<Result<Vec<_>>>()?,
+
+                    // Cannot pass both/neither
+                    _ => bail!("must specify only ONE of --to and --num-accounts"),
+                };
+
                 let max_transactions = VM::<Network, Db>::MAXIMUM_CONFIRMED_TRANSACTIONS;
                 let per_account = amount / to.len() as u64;
+
+                // Generate a transaction for each address
                 let transactions = to
                     .iter()
                     .progress_count(to.len() as u64)
@@ -455,6 +481,7 @@ impl Commands {
                     .filter_map(Result::ok)
                     .collect::<Vec<_>>();
 
+                // Add the transactions into blocks
                 let num_blocks = add_transaction_blocks(&ledger, from, &transactions, max_transactions, &mut rng)?;
 
                 Ok(format!(
