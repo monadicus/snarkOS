e80c823a8854a6e1d03fc22d8cd2133ef6170900
ljedrz <ljedrz@users.noreply.github.com>
2025-07-01 10:19:32 +0200

    docs: add a doc comment for the Resolver
    Signed-off-by: ljedrz <ljedrz@users.noreply.github.com>

--- node/router/src/helpers/resolver.rs
+++ node/router/src/helpers/resolver.rs
@@ -15,6 +15,9 @@
 
 use std::{collections::HashMap, net::SocketAddr};
 
+/// The `Resolver` provides the means to map the connected address (used in the lower-level
+/// `tcp` module internals, and provided by the OS) to the listener address (used as the
+/// unique peer identifier in the higher-level functions), and the other way around.
 #[derive(Debug, Default)]
 pub struct Resolver {
     /// The map of the listener address to (ambiguous) peer address.
