0e9f2cb5793dae05218e61f15cf1badb4a1bc06a
Kai Mast <kai@kaimast.com>
2025-07-14 16:23:23 -0700

    test(sync): add tests for rangify_heights

--- node/sync/src/block_sync/helpers.rs
+++ node/sync/src/block_sync/helpers.rs
@@ -58,7 +58,47 @@ mod tests {
     use super::rangify_heights;
 
     #[test]
-    fn test_rangify_heights() {
+    fn test_rangify_heights_empty() {
+        let heights = &[];
+
+        let rangified = rangify_heights(heights);
+        assert_eq!(rangified, "[]");
+    }
+
+    #[test]
+    fn test_rangify_large_continuous() {
+        let start = 16353;
+        let end = start + 52414;
+
+        let heights: Vec<u32> = (start..=end).collect();
+
+        let rangified = rangify_heights(&heights);
+        assert_eq!(rangified, format!("[{start}-{end}]"));
+    }
+
+    #[test]
+    fn test_rangify_many_small() {
+        let mut heights = vec![];
+        let mut expected = vec![];
+
+        for idx in 0..100 {
+            let start = idx * 4;
+            let mid = idx * 4 + 1;
+            let end = idx * 4 + 2;
+            expected.push(format!("{start}-{end}"));
+            heights.push(start);
+            heights.push(mid);
+            heights.push(end);
+        }
+
+        let rangified = rangify_heights(&heights);
+        let expected = format!("[{}]", expected.join(", "));
+
+        assert_eq!(rangified, expected);
+    }
+
+    #[test]
+    fn test_rangify_heights_multiple_ranges() {
         let heights = &[0, 1, 3, 4, 5, 6, 7, 10, 11, 12, 20, 22, 23, 24, 25, 27, 28];
 
         let rangified = rangify_heights(heights);
